650caf552d1b4683c74816c6be41661365a06106
dianabogdanova <dbogda15@yandex.ru>
2023-03-29 06:00:43 +0500

    initial commit

---
+++ src/test/java/me/dbogda/sockswarehouseapplication/SocksWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dbogda.sockswarehouseapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
